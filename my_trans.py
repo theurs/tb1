bc609e36fb187c3138879f3c632f5d5f503cdbf8
theurs <theurs@gmail.com>
2023-05-21 23:03:44 +1000

    fix

--- my_trans.py
+++ my_trans.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+
 from py_trans import PyTranslator
 from langdetect import detect, detect_langs
 import subprocess
+import gpt_basic
 
 
 def detect_lang(text):
@@ -54,12 +56,31 @@ def translate(text):
     if d and d != 'ru':
         # этот вариант почему то заметно хуже работает, хотя вроде бы тот же самый гугл переводчик
         #return translate_text(text)
-        return translate_text2(text)
+
+        #у этого варианта есть проблемы с кодировками в докере Ж)
+        #return translate_text2(text)
+        
+        return gpt_basic.translate_text(text) or translate_text2(text) or None
     return None
     
 
 if __name__ == "__main__":
-    text_de="""Звичайно, я можу написати два речення на українській мові."""
+    #text="""Звичайно, я можу написати два речення на українській мові."""
+    
+    
+    text = """Тарифи на електроенергію збільшуватимуть лише через пошкоджену енергосистему
+
+Міненерго шукає різні механізми для відновлення пошкодженої ворогом енергосистеми, заявив Фарід Сафаров, заступник міністра енергетики. 
+
+За його словами, наразі в енергетиці існує велика кількість проблем і потрібно знайти механізми, щоб у енергокомпаній були кошти на ремонти. Він підкреслив, що внаслідок атак було пошкоджено 66% теплоелектроцентралей. 
+
+Наразі Міністерство аналізує, наскільки необхідно підняти тариф для населення, щоб у енергетиків вистачило грошей на відновлення.
+ㅤ (https://t.me/+UATyHogngJZiMzZi)
+Надіслати новину @novosti_kieva_bot"""
+    
     #print(detect_lang('пато hello how are you going'))
-    print(translate_text(text_de))
-    print(translate_text2(text_de))
+    
+    #print(translate_text(text))
+    #print(translate_text2(text))
+
+    print(translate(text))

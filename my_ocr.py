252d946bd33243a3db26416235daed95d185e276
theurs <theurs@gmail.com>
2023-05-26 22:40:32 +1000

    1

--- my_ocr.py
+++ my_ocr.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+import tempfile
+import subprocess
+import os
 import io
 import re
 import fitz
@@ -8,6 +11,9 @@ import pytesseract
 from PIL import Image
 import enchant
 import gpt_basic
+
+
+pt_cmd = "/home/ubuntu/.local/bin/pytesseract"
 
 
 def replace_non_letters_with_spaces(text):
@@ -68,9 +74,30 @@ def get_text(fileobj):
     return text
 
 
+
+def ocr(input_file):
+    """распознает текст из файла"""
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        output_file = temp_file.name
+    
+    langs = 'eng'
+
+    with open(output_file, "w") as f:
+        subprocess.run([pt_cmd, "-l", langs, input_file], stdout = f)
+
+    with open(output_file, "r") as f:
+        text = f.read()
+    # Удаление временного файла
+    os.remove(output_file)
+    return text
+
+
+
+
 if __name__ == '__main__':
     pass
 
+    print(ocr('1.jpg'))
 
     #text = replace_non_letters_with_spaces("""one two три
     #                                       раз два опять

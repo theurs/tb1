633fa89d1738a02b2ca5fbbd90bcac30207ceb40
theurs <theurs@gmail.com>
2023-06-02 12:47:58 +1000

    fix

--- tb.py
+++ tb.py
@@ -503,7 +503,7 @@ def tts_thread(message: telebot.types.Message):
             return
         text = ' '.join(args)
         with show_action(message.chat.id, 'record_audio'):
-            audio = my_tts.tts(text, lang, rate)
+            audio = my_tts.tts(text, 'ru', rate)
             if message.reply_to_message:
                 bot.send_voice(message.chat.id, audio, reply_to_message_id = message.reply_to_message.message_id)
             else:

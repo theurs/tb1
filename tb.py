c983e0c05352a155b76a11eff35516bd5773b04d
theurs <theurs@gmail.com>
2023-06-02 12:41:24 +1000

    fix

--- tb.py
+++ tb.py
@@ -315,7 +315,7 @@ def handle_document_thread(message: telebot.types.Message):
                         print(error)
                     # Озвучиваем текст
                     audio = my_tts.tts(text, lang)
-                    if message.reply_to_message:
+                    if message.chat.type != 'private':
                         bot.send_voice(message.chat.id, audio, reply_to_message_id=message.reply_to_message.message_id)
                     else:
                         bot.send_voice(message.chat.id, audio)
@@ -344,7 +344,7 @@ def handle_document_thread(message: telebot.types.Message):
                     # если текст слишком длинный, отправляем его в виде текстового файла
                     if len(text) > 4096:
                         with io.StringIO(text) as f:
-                            if message.reply_to_message:
+                            if message.chat.type != 'private':
                                 bot.send_document(message.chat.id, document = f, visible_file_name = 'text.txt', caption='text.txt', reply_to_message_id = message.reply_to_message.id)
                             else:
                                 bot.send_document(message.chat.id, document = f, visible_file_name = 'text.txt', caption='text.txt')
@@ -392,10 +392,13 @@ def handle_photo_thread(message: telebot.types.Message):
                 if len(text) > 4096:
                     with io.StringIO(text) as f:
                         f.name = 'text.txt'
-                        bot.send_document(message.chat.id, f, reply_to_message_id=message.reply_to_message.message_id)
+                        if message.chat.type != 'private':
+                            bot.send_document(message.chat.id, f, reply_to_message_id=message.reply_to_message.message_id)
+                        else:
+                            bot.send_document(message.chat.id, f)
                         my_log.log(message, '[OCR] Sent as file: ' + text)
                 else:
-                    bot.send_message(message.chat.id, text, message.reply_to_message.message_id)
+                    bot.reply_to(message, text)
                     my_log.log(message, '[OCR] ' + text)
             else:
                 my_log.log(message, '[OCR] no results')
@@ -414,7 +417,7 @@ def handle_video_thread(message: telebot.types.Message):
             # у видео нет текста но есть заголовок caption. его и будем переводить
             text = my_trans.translate(message.caption)
             if text:
-                bot.send_message(message.chat.id, text)
+                bot.reply_to(message, text)
                 my_log.log(message, text)
             else:
                 my_log.log(message, "")

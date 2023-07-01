3ec59f28b0f4356f92e068eefabb8958a0b8ca3a
theurs <theurs@gmail.com>
2023-07-02 01:28:12 +1000

    fix

--- my_dic.py
+++ my_dic.py
@@ -2,11 +2,15 @@
 
 
 import pickle
+import threading
+from pprint import pprint
 
 
 class PersistentDict(dict):
-    """Словарь который хранит состояние в файле на диске, данные сохраняются между перезапусками программы"""
+    """Словарь который хранит состояние в файле на диске, данные сохраняются между
+    перезапусками программы"""
     def __init__(self, file_path):
+        self.lock = threading.Lock()
         self.file_path = file_path
         try:
             with open(self.file_path, 'rb') as f:
@@ -18,48 +22,54 @@ class PersistentDict(dict):
             self.update(data)
         except FileNotFoundError:
             pass
-    
+
     def __setitem__(self, key, value):
         super().__setitem__(key, value)
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(dict(self), f)
-    
+        with self.lock:
+            with open(self.file_path, 'wb') as f:
+                pickle.dump(dict(self), f)
+
     def __delitem__(self, key):
         super().__delitem__(key)
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(dict(self), f)
-    
+        with self.lock:
+            with open(self.file_path, 'wb') as f:
+                pickle.dump(dict(self), f)
+
     def clear(self):
         super().clear()
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(dict(self), f)
-    
+        with self.lock:
+            with open(self.file_path, 'wb') as f:
+                pickle.dump(dict(self), f)
+
     def pop(self, key, default=None):
         value = super().pop(key, default)
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(dict(self), f)
+        with self.lock:
+            with open(self.file_path, 'wb') as f:
+                pickle.dump(dict(self), f)
         return value
-    
+
     def popitem(self):
         item = super().popitem()
-        with open(self.file_path, 'rb') as f:
-            pickle.dump(dict(self), f)
+        with self.lock:
+            with open(self.file_path, 'rb') as f:
+                pickle.dump(dict(self), f)
         return item
-    
+
     def setdefault(self, key, default=None):
         value = super().setdefault(key, default)
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(dict(self), f)
+        with self.lock:
+            with open(self.file_path, 'wb') as f:
+                pickle.dump(dict(self), f)
         return value
-    
+
     def update(self, E=None, **F):
         super().update(E, **F)
-        with open(self.file_path, 'wb') as f:
-            pickle.dump(dict(self), f)
+        with self.lock:
+            with open(self.file_path, 'wb') as f:
+                pickle.dump(dict(self), f)
 
 
 
 if __name__ == '__main__':
-    my_dict = PersistentDict('dialogs.pkl')
-    my_dict['key'] = 'value'
-    print(my_dict)
+    my_dict = PersistentDict('db/dialogs.pkl')
+    pprint(my_dict)

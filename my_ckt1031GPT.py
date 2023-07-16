91e24195f7a09e3e000e8457777fe63ab97c0157
theurs <theurs@gmail.com>
2023-07-16 13:37:21 +1000

    fix

--- my_ckt1031GPT.py
+++ my_ckt1031GPT.py
@@ -10,33 +10,6 @@ import openai
 import cfg
 
 
-
-def ai_test() -> str:
-    """
-    """
-    openai.api_key = cfg.key_test
-    openai.api_base = cfg.openai_api_base_test
-    
-    #   text = open('1.txt', 'r').read()[:20000]
-    text = 'Привет как дела'
-    
-    messages = [{"role": "system", "content": "Ты искусственный интеллект отвечающий на запросы юзера."},
-                {"role": "user", "content": text}]
-
-    current_model = cfg.model_test
-
-    # тут можно добавить степень творчества(бреда) от 0 до 1 дефолт - temperature=0.5
-    сompletion = openai.ChatCompletion.create(
-        model = current_model,
-        messages=messages,
-        max_tokens=2000,
-        temperature=0.5,
-        timeout=180,
-        stream=False
-    )
-    return сompletion["choices"][0]["message"]["content"]
-
-
 def ai(prompt: str = '', temp: float = 0.5, max_tok: int = 2000, timeou: int = 180, messages = None, chat_id = None) -> str:
     """Сырой текстовый запрос к GPT чату, возвращает сырой ответ
     second - использовать ли второй гейт и ключ, для больших запросов
@@ -99,13 +72,3 @@ if __name__ == '__main__':
     for _ in range(20):
         print(ai('привет. одна цифра и вопрос в ответе должна быть и никаких слов. вопрос - 1+2='))
     #print(ai(open('1.txt','r', encoding='utf-8').read()[:25000]))
-    sys.exit()
-
-    if len(sys.argv) != 2:
-        print("Usage: gptbasic.py filename|'request to qpt'")
-        sys.exit(1)
-    t = sys.argv[1]
-    if os.path.exists(t):
-        print(ai(open(t).read(), max_tok = 2000, second = True))
-    else:
-        print(ai(t, max_tok = 2000))

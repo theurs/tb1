42423b7e0e399de3d0d7fbac2b2eae35fa5590c8
theurs <theurs@gmail.com>
2023-08-07 11:24:37 +1000

    fix

--- tb.py
+++ tb.py
@@ -1970,7 +1970,10 @@ def reply_to_long_message(message: telebot.types.Message, resp: str, parse_mode:
                           disable_web_page_preview: bool = None, reply_markup: telebot.types.InlineKeyboardMarkup = None):
     """отправляем сообщение, если оно слишком длинное то разбивает на 2 части либо отправляем как текстовый файл"""
     if len(resp) < 20000:
-        chunks = utils.split_text(resp, 3500)
+        if parse_mode == 'HTML':
+            chunks = utils.split_html(resp, 4000)
+        else:
+            chunks = utils.split_text(resp, 4000)
         counter = len(chunks)
         for chunk in chunks:
             try:

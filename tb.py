c22251b519ce9e7c6ba15fe6afd3e7522f421d28
theurs <theurs@gmail.com>
2023-11-09 19:20:58 +1000

    fix. translate image prompts

--- tb.py
+++ tb.py
@@ -1696,7 +1696,7 @@ def image_thread(message: telebot.types.Message):
                 moderation_flag = gpt_basic.moderation(prompt)
                 prompt_tr = ''
                 try:
-                    prompt_tr = gpt_basic.ai_instruct(f'Translate into english: {prompt}')
+                    prompt_tr = gpt_basic.ai_instruct(f'Translate into english if it is not english, else leave it as it is: {prompt}')
                 except Exception as image_prompt_translate:
                     my_log.log2(f'image:translate_prompt: {str(image_prompt_translate)}\n\n{prompt}')
                 if not prompt_tr:

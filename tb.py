2d484aaa69fe360e0bb5e4083d363d90cafc773b
theurs <theurs@gmail.com>
2023-12-22 15:26:06 +1000

    fix

--- tb.py
+++ tb.py
@@ -1360,7 +1360,8 @@ def handle_photo_thread(message: telebot.types.Message):
 
     msglower = message.caption.lower() if message.caption else ''
 
-    if (tr('что', lang) in msglower and len(msglower) < 30) or msglower == '':
+    # if (tr('что', lang) in msglower and len(msglower) < 30) or msglower == '':
+    if msglower.startswith('?'):
         state = 'describe'
     # elif 'ocr' in msglower or tr('прочитай', lang) in msglower or tr('читай', lang) in msglower:
     elif 'ocr' in msglower:
@@ -2835,7 +2836,7 @@ In private messages, you don't need to mention the bot's name
 
 🔭 If you send a link in a private message, the bot will try to extract and provide a brief summary of the content.
 
-🛸 To get text from an image, send the image with the caption "ocr".
+🛸 To get text from an image, send the image with the caption "ocr". Send message with caption starting ? for image describe.
 
 🎙️ You can issue commands and make requests using voice messages.
 

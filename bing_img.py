12caeef02a096afb95cfce1a08feef7053397a99
theurs <theurs@gmail.com>
2024-01-05 15:47:26 +1000

    fix

--- bing_img.py
+++ bing_img.py
@@ -213,6 +213,7 @@ def gen_images(query: str, custom_proxies = None, remove_auto_proxies = False) -
             unsuspend = [x[0] for x in COOKIE_SUSPENDED.items() if time.time() > x[1] + SUSPEND_COOKIE_TIME]
             for x in unsuspend:
                 COOKIE[time.time()] = x
+                COOKIE_SUSPENDED.pop(x)
             for x in COOKIE.items():
                 cookie = x[1].strip()
                 cookies.append(cookie)

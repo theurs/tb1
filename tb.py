80d751bf01dc6d4fc98622d6183ba796c70e862a
theurs <theurs@gmail.com>
2024-01-15 13:51:23 +1000

    fix

--- tb.py
+++ tb.py
@@ -2848,7 +2848,7 @@ def stats_thread(message: telebot.types.Message):
                 my_log.log2(f'tb:stats_thread:add_row {unknown}')
 
     for sorting in ['left messages', 'left days', 'lang', 'chat mode', 'images']:
-        
+        bot_reply(message, sorting)
         users_text = f'{tr("Usage statistics sorted by:", lang)} {sorting}\n\n<pre><code>{pt.get_string(sortby=sorting)}</code></pre>'
         users_text += f'\n\n{tr("Total:", lang)} {str(len(users_sorted))}'
         bot_reply(message, users_text, parse_mode='HTML')
--- tb_lite.py
+++ tb_lite.py
@@ -10,10 +10,10 @@ bot = telebot.TeleBot(cfg.token, skip_pending=True)
 
 @bot.message_handler(commands=['start'])
 def command_code(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'https://mail.ru', parse_mode='HTML',
-                     disable_web_page_preview=True)
-                    #  link_preview_options=telebot.types.LinkPreviewOptions(is_disabled=False))
-
+    bot.send_message(message.chat.id, 'https://habr.com/ru/articles/786352/', parse_mode='HTML',
+                     link_preview_options=telebot.types.LinkPreviewOptions(is_disabled=False, show_above_text=True))
+    bot.send_message(message.chat.id, 'https://habr.com/ru/articles/786352/', parse_mode='HTML',
+                     link_preview_options=telebot.types.LinkPreviewOptions(is_disabled=False, show_above_text=False))
 
 if __name__ == '__main__':
     bot.polling()

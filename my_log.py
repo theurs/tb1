f78aca32798656becb6ca05089926282ac1fb6d6
theurs <theurs@gmail.com>
2024-01-27 16:50:33 +1000

    add cfg.LOGS_GROUP

--- my_log.py
+++ my_log.py
@@ -7,6 +7,8 @@ import datetime
 import telebot
 import threading
 
+import cfg
+
 
 lock = threading.Lock()
 
@@ -17,88 +19,137 @@ if not os.path.exists('logs2'):
     os.mkdir('logs2')
 
 
+# -1 - do not log to files
+# 0 - log users to log2/ only
+# 1 - log users to log/ and log2/
+LOG_MODE = cfg.LOG_MODE if hasattr(cfg, 'LOG_MODE') else 0
+
+
 def log2(text: str) -> None:
     """для дебага"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (0,1):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_trial(text: str) -> None:
     """для сообщений о триале"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_trials.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_bing_success(text: str) -> None:
     """для логов удачных комбинаций бинга, когда ему удалось нарисовать что-нибудь"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_bing_success.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_huggin_face_api(text: str) -> None:
     """для логов от hugging_face_api"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_hugging_face_api.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_parser_error(text: str) -> None:
     """для дебага ошибок md->html конвертера"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_md2html.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_bing_img(text: str) -> None:
     """для дебага ошибок bing_img с помощью ai"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_bing_img.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_stable_diffusion_api(text: str) -> None:
     """для дебага ошибок stable_diffusion_api с помощью ai"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_stable_diffusion_api.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_translate(text: str) -> None:
     """для дебага ошибок автоперевода с помощью ai"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     with lock:
         time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
         log_file_path = 'logs/debug_translate.log'
-        open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
-        open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,):
+            open(log_file_path, 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
+        if LOG_MODE in (1,0):
+            open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8").write(f'{time_now}\n\n{text}\n{"=" * 80}\n')
 
 
 def log_echo(message: telebot.types.Message, reply_from_bot: str = '', debug: bool = False) -> None:
     """записывает в журнал сообщение полученное обработчиком обычных сообщений либо ответ бота"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     private_or_chat = 'private' if message.chat.type == 'private' else 'chat'
@@ -125,21 +176,25 @@ def log_echo(message: telebot.types.Message, reply_from_bot: str = '', debug: bo
         log_file_path = log_file_path[:-4] + f' [{topic_id}].log'
 
     with lock:
-        with open(log_file_path, 'a', encoding="utf-8") as log_file:
-            if reply_from_bot:
-                log_file.write(f"[{time_now}] [BOT]: {reply_from_bot}\n")
-            else:
-                log_file.write(f"[{time_now}] [{user_name}]: {message.text or message.caption or ''}\n")
-        with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
-            if reply_from_bot:
-                log_file.write(f"[{time_now}] [BOT]: {reply_from_bot}\n")
-            else:
-                log_file.write(f"[{time_now}] [{user_name}]: {message.text or message.caption or ''}\n")
-
+        if LOG_MODE in (1,):
+            with open(log_file_path, 'a', encoding="utf-8") as log_file:
+                if reply_from_bot:
+                    log_file.write(f"[{time_now}] [BOT]: {reply_from_bot}\n")
+                else:
+                    log_file.write(f"[{time_now}] [{user_name}]: {message.text or message.caption or ''}\n")
+        if LOG_MODE in (0,1):
+            with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
+                if reply_from_bot:
+                    log_file.write(f"[{time_now}] [BOT]: {reply_from_bot}\n")
+                else:
+                    log_file.write(f"[{time_now}] [{user_name}]: {message.text or message.caption or ''}\n")
 
 
 def log_media(message: telebot.types.Message) -> None:
     """записывает в журнал сообщение полученное обработчиком медиа файлов"""
+    if LOG_MODE == -1:
+        return
+
     global lock
     time_now = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
     private_or_chat = 'private' if message.chat.type == 'private' else 'chat'
@@ -171,53 +226,66 @@ def log_media(message: telebot.types.Message) -> None:
         file_title = message.audio.title
         file_mime_type = message.audio.mime_type
         with lock:
-            with open(log_file_path, 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил аудио файл] [caption: {caption}] [title: {file_title}] \
-[filename: {file_name}] [filesize: {file_size}] [duration: {file_duration}] [mime type: {file_mime_type}]\n")
-            with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил аудио файл] [caption: {caption}] [title: {file_title}] \
-[filename: {file_name}] [filesize: {file_size}] [duration: {file_duration}] [mime type: {file_mime_type}]\n")
+            if LOG_MODE in (1,):
+                with open(log_file_path, 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил аудио файл] [caption: {caption}] [title: {file_title}] \
+    [filename: {file_name}] [filesize: {file_size}] [duration: {file_duration}] [mime type: {file_mime_type}]\n")
+            if LOG_MODE in (0,1):
+                with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил аудио файл] [caption: {caption}] [title: {file_title}] \
+    [filename: {file_name}] [filesize: {file_size}] [duration: {file_duration}] [mime type: {file_mime_type}]\n")
 
     if message.voice:
         file_size = message.voice.file_size
         file_duration = message.voice.duration
         with lock:
-            with open(log_file_path, 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил голосовое сообщение] [filesize: \
-{file_size}] [duration: {file_duration}]\n")
-            with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил голосовое сообщение] [filesize: \
-{file_size}] [duration: {file_duration}]\n")
+            if LOG_MODE in (1,):
+                with open(log_file_path, 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил голосовое сообщение] [filesize: \
+    {file_size}] [duration: {file_duration}]\n")
+            if LOG_MODE in (0,1):
+                with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил голосовое сообщение] [filesize: \
+    {file_size}] [duration: {file_duration}]\n")
 
     if message.document:
         file_name = message.document.file_name
         file_size = message.document.file_size
         file_mime_type = message.document.mime_type
         with lock:
-            with open(log_file_path, 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил документ] [caption: {caption}] \
-[filename: {file_name}] [filesize: {file_size}] [mime type: {file_mime_type}]\n")
-            with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил документ] [caption: {caption}] \
-[filename: {file_name}] [filesize: {file_size}] [mime type: {file_mime_type}]\n")
+            if LOG_MODE in (1,):
+                with open(log_file_path, 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил документ] [caption: {caption}] \
+    [filename: {file_name}] [filesize: {file_size}] [mime type: {file_mime_type}]\n")
+            if LOG_MODE in (0,1):
+                with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил документ] [caption: {caption}] \
+    [filename: {file_name}] [filesize: {file_size}] [mime type: {file_mime_type}]\n")
 
     if message.photo or message.video:
-        with lock:
-            with open(log_file_path, 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил фото] [caption]: {caption}\n")
-        with lock:
-            with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
-                log_file.write(f"[{time_now}] [{user_name}]: [Отправил фото] [caption]: {caption}\n")
+        if LOG_MODE in (1,):
+            with lock:
+                with open(log_file_path, 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил фото] [caption]: {caption}\n")
+        if LOG_MODE in (0,1):
+            with lock:
+                with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
+                    log_file.write(f"[{time_now}] [{user_name}]: [Отправил фото] [caption]: {caption}\n")
 
 
 def log_google(request: str, respond: str):
     """записывает в журнал сообщение полученное обработчиком google"""
+    if LOG_MODE == -1:
+        return
+
     time_now = datetime.datetime.now().strftime('%d-%m-%Y %H.%M.%S')
     log_file_path = f'logs/askgoogle at {time_now}.log'
-    with open(log_file_path, 'a', encoding="utf-8") as log_file:
-        log_file.write(f'{respond}\n\n{"="*40}\n\n{request}')
-    with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
-        log_file.write(f'{respond}\n\n{"="*40}\n\n{request}')
+    if LOG_MODE in (1,):
+        with open(log_file_path, 'a', encoding="utf-8") as log_file:
+            log_file.write(f'{respond}\n\n{"="*40}\n\n{request}')
+    if LOG_MODE in (0,1):
+        with open(log_file_path.replace('logs/', 'logs2/', 1), 'a', encoding="utf-8") as log_file:
+            log_file.write(f'{respond}\n\n{"="*40}\n\n{request}')
 
 
 def purge(chat_id: int) -> bool:
@@ -238,7 +306,7 @@ def purge(chat_id: int) -> bool:
         return False
     return True
 
+
 if __name__ == '__main__':
     pass
     purge(1651196)
-    

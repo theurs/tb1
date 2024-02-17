21533cafc0651bd8b956a131a880077bdccd6765
theurs <theurs@gmail.com>
2024-02-17 23:01:51 +1000

    fix

--- my_log.py
+++ my_log.py
@@ -187,6 +187,7 @@ def log_echo(message: telebot.types.Message, reply_from_bot: str = '', debug: bo
     user_name = message.from_user.first_name or message.from_user.username or ''
     chat_name = chat_name.replace('/', '⁄')
     user_name = user_name.replace('/', '⁄')
+    user_name = f'{user_name} {message.from_user.id}'
 
     logname = f'logs/[{chat_name}] [{private_or_chat}] [{message.chat.type}] [{message.chat.id}].log'.replace('[private] [private]', '[private]').replace('[chat] [supergroup]', '[chat]')
 

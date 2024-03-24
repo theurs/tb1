28003b702798ef0028f7a690b7b9891cefee1edf
theurs <theurs@gmail.com>
2024-03-24 14:20:19 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -270,14 +270,8 @@ def huggin_face_api(prompt: str) -> bytes:
         API_URL = cfg.huggin_face_models_urls
     else:
         API_URL = [
-            # "https://api-inference.huggingface.co/models/stablediffusionapi/juggernaut-xl-v8",
-            # "https://api-inference.huggingface.co/models/stablediffusionapi/juggernaut-xl-v8",
-            # "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl",
             "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl-v2",
-            # "https://api-inference.huggingface.co/models/ehristoforu/dalle-3-xl-v2",
             'AP123/SDXL-Lightning',
-            #'AP123/SDXL-Lightning',
-            # 'playgroundai/playground-v2.5-1024px-aesthetic',
             'playgroundai/playground-v2.5-1024px-aesthetic',
             "multimodalart/stable-cascade",
             "https://api-inference.huggingface.co/models/digiplay/Juggernaut_final",
@@ -334,7 +328,7 @@ def huggin_face_api(prompt: str) -> bytes:
             #     print(resp_text[:300])
             if 'read timeout=' in resp_text or "SOCKSHTTPSConnectionPool(host='api-inference.huggingface.co', port=443): Max retries exceeded with url" in resp_text: # и так долго ждали
                 return []
-            if response.content and '{"error"' not in resp_text:
+            if response.content and '{"error"' not in resp_text and len(response.content) > 5000:
                 result.append(response.content)
                 WHO_AUTOR[hash(response.content)] = url.split('/')[-1]
                 return result

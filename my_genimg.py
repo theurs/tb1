0b4c333b0b562c741c0189401c9385afdc26b41a
theurs <theurs@gmail.com>
2024-04-01 20:51:00 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -694,7 +694,7 @@ def gen_images(prompt: str, moderation_flag: bool = False, user_id: str = '', co
     async_result5 = pool.apply_async(stability_ai, (prompt,))
     async_result6 = pool.apply_async(stability_ai, (prompt,))
 
-    result = async_result1.get() + async_result2.get() + async_result3.get() + async_result4.get() + async_result5.get() + async_result6.get()
+    result = async_result1.get() + async_result5.get() + async_result6.get() + async_result2.get() + async_result3.get() + async_result4.get()
 
     # пытаемся почистить /tmp от временных файлов которые создает stable-cascade?
     # может удалить то что рисуют параллельные запросы и второй бот?

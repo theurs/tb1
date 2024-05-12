4283bbd774adb6a6e6cad5ac397bc9a967c0a730
theurs <theurs@gmail.com>
2024-05-12 23:27:33 +1000

    fix

--- my_sum.py
+++ my_sum.py
@@ -100,9 +100,9 @@ BEGIN:
         try:
             
             if subj == 'youtube_video':
-                qq = f'Summarize the content of this YouTube video using only the subtitles, what this video about, in no more than 1000 words, answer in [{lang}] language.'
+                qq = f'Summarize the content of this YouTube video using only the subtitles, what this video about, in no more than 2000 words, answer in [{lang}] language.'
             else:
-                qq = f'Summarize the content of this article using only provided text, what this text about, in no more than 1000 words, answer in [{lang}] language.'
+                qq = f'Summarize the content of this article using only provided text, what this text about, in no more than 2000 words, answer in [{lang}] language.'
             if query:
                 qq = query
             r = my_gemini.sum_big_text(text[:my_gemini.MAX_SUM_REQUEST], qq).strip()

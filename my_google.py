3e1550a9478a02dbf4288bce523e23ebbe90753c
theurs <theurs@gmail.com>
2024-05-13 23:17:18 +1000

    fix

--- my_google.py
+++ my_google.py
@@ -11,6 +11,7 @@ import trafilatura
 import cfg
 import my_log
 import my_gemini
+import my_groq
 import my_trans
 
 
@@ -293,6 +294,90 @@ Search results:
     return my_gemini.ai(q, model='gemini-1.5-pro-latest')
 
 
+def search_v4(query: str, lang: str = 'ru', max_search: int = 10) -> str:
+    # добавляем в список выдачу самого гугла, и она же первая и главная
+    urls = [f'https://www.google.com/search?q={urllib.parse.quote(query)}',]
+    # добавляем еще несколько ссылок, возможно что внутри будут пустышки, джаваскрипт заглушки итп
+    try:
+        r = googlesearch.search(query, stop = max_search, lang=lang)
+    except Exception as error:
+        my_log.log2(f'my_google:search_google_v2: {error}')
+        try:
+            r = [x for x in ddg_text(query)]
+        except Exception as error:
+            my_log.log2(f'my_google:search_google_v2: {error}')
+            return ''
+
+    bad_results = ('https://g.co/','.pdf','.docx','.xlsx', '.doc', '.xls')
+
+    try:
+        for url in r:
+            if any(s.lower() in url.lower() for s in bad_results):
+                continue
+            urls.append(url)
+    except Exception as error:
+        error_traceback = traceback.format_exc()
+        my_log.log2(f'my_google:search_v3: {error}\n\n{error_traceback}')
+
+    text = ''
+    for url in urls:
+        # print(url)
+        text += download_text([url,], 5000)
+        if len(text) > 12000:
+            break
+
+    q = f'''Answer in "{lang}" language to users search query using search results and your own knowledge.
+User query: "{query}"
+
+Search results:
+
+{text[:12000]}
+'''
+    return my_groq.ai(q, max_tokens_ = 4000)
+
+
+def search_v5(query: str, lang: str = 'ru', max_search: int = 10) -> str:
+    # добавляем в список выдачу самого гугла, и она же первая и главная
+    urls = [f'https://www.google.com/search?q={urllib.parse.quote(query)}',]
+    # добавляем еще несколько ссылок, возможно что внутри будут пустышки, джаваскрипт заглушки итп
+    try:
+        r = googlesearch.search(query, stop = max_search, lang=lang)
+    except Exception as error:
+        my_log.log2(f'my_google:search_google_v2: {error}')
+        try:
+            r = [x for x in ddg_text(query)]
+        except Exception as error:
+            my_log.log2(f'my_google:search_google_v2: {error}')
+            return ''
+
+    bad_results = ('https://g.co/','.pdf','.docx','.xlsx', '.doc', '.xls')
+
+    try:
+        for url in r:
+            if any(s.lower() in url.lower() for s in bad_results):
+                continue
+            urls.append(url)
+    except Exception as error:
+        error_traceback = traceback.format_exc()
+        my_log.log2(f'my_google:search_v3: {error}\n\n{error_traceback}')
+
+    text = ''
+    for url in urls:
+        # print(url)
+        text += download_text([url,], 20000)
+        if len(text) > 31000:
+            break
+
+    q = f'''Answer in "{lang}" language to users search query using search results and your own knowledge.
+User query: "{query}"
+
+Search results:
+
+{text[:31000]}
+'''
+    return my_gemini.ai(q, model='gemini-1.5-pro-latest')
+
+
 if __name__ == "__main__":
     #text, links = shorten_links(text)
     #print(text)
@@ -309,7 +394,9 @@ if __name__ == "__main__":
     
     # print(search_v3('полный текст песни doni ft валерия ты такой'), '\n\n')
 
-    print(search_v3('перспектива наступления в харковщине'), '\n\n')
+    # print(search_v4('полный текст песни малиновая лада'), '\n\n')
+    # print(search_v5('когда доллар рухнет?'), '\n\n')
+    print(search_v5('текст песни егора пикачу'), '\n\n')
 
     # print(search('курс доллара'), '\n\n')
     # print(search('текст песни егора пикачу'), '\n\n')

582808feb59d37ec42b9c17f7b4970ff45ad0512
theurs <theurs@gmail.com>
2024-05-13 20:18:13 +1000

    fix

--- tb.py
+++ tb.py
@@ -537,7 +537,7 @@ def is_for_me(message: telebot.types.Message):
 
 def log_message(message: telebot.types.Message):
     try:
-        if hasattr(cfg, 'DO_NOT_LOG') and message.chat.id in cfg.DO_NOT_LOG:
+        if isinstance(message, telebot.types.Message) and hasattr(cfg, 'DO_NOT_LOG') and message.chat.id in cfg.DO_NOT_LOG:
             return
 
         if not hasattr(cfg, 'LOGS_GROUP') or not cfg.LOGS_GROUP:

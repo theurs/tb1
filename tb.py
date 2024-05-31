c37bad5bcd70b3e0dabe3f83bb7f2c3d5cd1fc48
theurs <theurs@gmail.com>
2024-05-31 21:37:19 +1000

    fix

--- tb.py
+++ tb.py
@@ -645,7 +645,7 @@ def log_group_daemon():
     group = 10
     while LOG_GROUP_DAEMON_ENABLED:
         try:
-            time.sleep(3 + group) # telegram limit 1 message per second for groups
+            time.sleep(2 + group) # telegram limit 1 message per second for groups
             group = 0
             with LOG_GROUP_MESSAGES_LOCK:
                 try:
@@ -689,7 +689,7 @@ def log_group_daemon():
                 elif _type == 'copy':
                     if _m_ids:
                         try:
-                            group = len(_m_ids)*3
+                            group = len(_m_ids)*2
                             bot.copy_messages(cfg.LOGS_GROUP, _message_chat_id, _m_ids, message_thread_id=th)
                         except Exception as error3_0:
                             try:

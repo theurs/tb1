6438f4fd29848a267940cee21bacf3e13ebf5dd0
theurs <theurs@gmail.com>
2024-06-10 09:38:50 +1000

    fix

--- utils.py
+++ utils.py
@@ -553,7 +553,7 @@ def get_codepage():
 if __name__ == '__main__':
     # print(get_codepage())
     l = 'sudo systemctl show wg-quick@wg1.service'
-    with subprocess.Popen(l, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as proc:
+    with subprocess.Popen(l, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding=get_codepage()) as proc:
         stdout, stderr = proc.communicate()
         print(stdout)
         print(stderr)

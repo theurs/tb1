6f73795d9e0520278a4c24610cc82a3d686f9e36
theurs <theurs@gmail.com>
2024-07-05 09:25:41 +1000

    fix

--- my_gemini.py
+++ my_gemini.py
@@ -49,10 +49,10 @@ TIMEOUT = 180
 
 LOCKS = {}
 CHATS = {}
-MAX_CHAT_LINES = 20
+MAX_CHAT_LINES = 50
 if hasattr(cfg, 'GEMINI_MAX_CHAT_LINES'):
     MAX_CHAT_LINES = cfg.GEMINI_MAX_CHAT_LINES
-MAX_CHAT_MEM_BYTES = 50000
+MAX_CHAT_MEM_BYTES = 150000
 # не принимать запросы больше чем, это ограничение для телеграм бота, в этом модуле оно не используется
 MAX_REQUEST = 25000
 MAX_SUM_REQUEST = 300000

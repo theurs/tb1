4edc58bc5dfb2d3cf4c8b386a1f6f67bf1d26ce0
theurs <theurs@gmail.com>
2024-07-21 22:03:33 +1000

    fix

--- my_sum.py
+++ my_sum.py
@@ -38,7 +38,7 @@ def get_subs_from_rutube(url: str) -> str:
         return cache
 
     duration = my_transcribe.get_url_video_duration(url)
-    if duration == 0 or duration > 6*60*60:
+    if duration == 0 or duration > 4*60*60:
         my_log.log2(f'my_sum:get_subs_from_rutube: too long video {url} {duration}')
         return ''
 

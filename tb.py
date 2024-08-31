b9771f81538a68e5e430176061d8e2b9422657c7
theurs <theurs@gmail.com>
2024-08-31 14:52:26 +1000

    fix

--- tb.py
+++ tb.py
@@ -5003,10 +5003,10 @@ def do_task(message, custom_prompt: str = ''):
         msg = message.text.lower()
 
 
-        # попробовать исправить раскладку ghbdtn->привет
-        if lang == 'ru' and not msg.startswith('/') and not msg.startswith('http') and len(message.text) < 500 and len(message.text) > 4:
-            message.text = my_correct_layout_ru.correct_layout(message.text)
-            msg = message.text.lower()
+        # # попробовать исправить раскладку ghbdtn->привет
+        # if lang == 'ru' and not msg.startswith('/') and not msg.startswith('http') and len(message.text) < 500 and len(message.text) > 4:
+        #     message.text = my_correct_layout_ru.correct_layout(message.text)
+        #     msg = message.text.lower()
 
 
         # если предварительно была введена какая то команда то этот текст надо отправить в неё
@@ -5717,7 +5717,7 @@ def main():
     my_groq.load_users_keys()
     my_trans.load_users_keys()
 
-    my_correct_layout_ru.my_correct_layout_ru_v2.load()
+    # my_correct_layout_ru.my_correct_layout_ru_v2.load()
 
     one_time_shot()
 

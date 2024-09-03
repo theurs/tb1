56f02c4b144703cb9725c8d5661e85484945de1e
theurs <theurs@gmail.com>
2024-09-03 18:05:11 +1000

    fix

--- my_genimg.py
+++ my_genimg.py
@@ -1148,6 +1148,7 @@ def gen_images(prompt: str, moderation_flag: bool = False,
 
     negative = ''
 
+    reprompt = ''
     if use_bing:
         reprompt, negative = get_reprompt(prompt, conversation_history)
 

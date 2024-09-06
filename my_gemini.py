afa3f88524e8128d814d5b45489e626641fc8581
theurs <theurs@gmail.com>
2024-09-06 22:54:15 +1000

    fix

--- my_gemini.py
+++ my_gemini.py
@@ -233,7 +233,7 @@ def chat(query: str,
                 #     ALL_KEYS = [x for x in ALL_KEYS if x != key]
                 #     my_log.log_gemini(f'Suspend key {key}, active left keys: {len(ALL_KEYS)}')
                 if 'reason: "CONSUMER_SUSPENDED"' in str(error) or \
-                   'API key expired. Please renew the API key.' in str(error):
+                   'reason: "API_KEY_INVALID"' in str(error):
                     remove_key(key)
                 if 'finish_reason: ' in str(error) or 'block_reason: ' in str(error) or 'User location is not supported for the API use.' in str(error):
                     return ''

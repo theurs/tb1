af84c06bffc68832eb03abc784ff5f6f8d4f355c
theurs <theurs@gmail.com>
2024-09-22 12:49:34 +1000

    fix

--- my_telegraph.py
+++ my_telegraph.py
@@ -21,7 +21,7 @@ def post(text: str, user_id: str) -> str:
             telegraph = Telegraph(access_token=token)
         else:
             telegraph = Telegraph()
-            telegraph.create_account(short_name='kun4sun_bot', author_name=user_id, author_url='https://t.me/kun4sun_bot')
+            telegraph.create_account(short_name='kun4sun_bot', author_name='kun4sun_bot', author_url='https://t.me/kun4sun_bot')
             token = telegraph.get_access_token()
             if token:
                 TOKENS[user_id] = token
@@ -29,7 +29,7 @@ def post(text: str, user_id: str) -> str:
                 return None
 
         response = telegraph.create_page(
-            f'kun4sun_bot - {user_id}',
+            f'kun4sun_bot - {str(hash(user_id))[:-8]}',
             html_content=text,
         )
         return response['url']
@@ -42,20 +42,7 @@ def post(text: str, user_id: str) -> str:
 if __name__ == '__main__':
     pass
     p1 = '''
-<table border="1" cellpadding="5" cellspacing="2">
-  <tr>
-    <th>Заголовок 1</th>
-    <th>Заголовок 2</th>
-  </tr>
-  <tr>
-    <td>Данные 1</td>
-    <td>Данные 2</td>
-  </tr>
-  <tr>
-    <td>Данные 3</td>
-    <td>Данные 4</td>
-  </tr>
-</table>
+hi
 
 '''
     p2 = '<p>!!!Этот абзац содержит!!! <strong>много текста</strong>, включая <a href="https://example.com">ссылку</a> и <em>выделенный текст</em>. Он также содержит специальный символ &amp; и тег изображения <img src="image.jpg" alt="Пример изображения"></p>'

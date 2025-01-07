6948f77763e096aa3f85925059e924e8f60ca563
theurs <theurs@gmail.com>
2025-01-08 00:33:10 +1000

    fix

--- tb.py
+++ tb.py
@@ -1711,7 +1711,7 @@ def get_keyboard(kbd: str, message: telebot.types.Message, flag: str = '') -> te
             markup.add(button0, button1, button2, button3, button4)
             return markup
         elif kbd.startswith('search_pics_'):
-            markup  = telebot.types.InlineKeyboardMarkup(row_width=3)
+            markup  = telebot.types.InlineKeyboardMarkup(row_width=4)
             button0 = telebot.types.InlineKeyboardButton('📸', callback_data=f'search_pics_{kbd[12:]}')
             button1 = telebot.types.InlineKeyboardButton("🙈", callback_data='erase_answer')
             button2 = telebot.types.InlineKeyboardButton("📢", callback_data='tts')

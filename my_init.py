3c65c0c1f72f76cbf081bc64cf5188588510358c
theurs <theurs@gmail.com>
2025-01-14 13:07:06 +1000

    fix

--- my_init.py
+++ my_init.py
@@ -189,7 +189,7 @@ def get_hidden_prompt_for_user(message, chat_id_full, bot_name, lang_of_user, fo
                     'you can generate images with the /img command, type /img "prompt" to generate an image for user, '
                     'you can google search with the /google command, type /google "question" to get the answer for user, '
                     'you can summarize text with the /sum command, type /sum URL to get the summary for user, '
-                    'you can say your answer with voice message with the /tts command, type /tts "text" to answer to user in voice, '
+                    'you can say your answer with voice message with the /tts command, type /tts <lang> TEXT to answer to user in voice, '
                     f'{"your special role here is " + my_db.get_user_property(chat_id_full, "role") + ", " if my_db.get_user_property(chat_id_full, "role") else ""}'
                 )
 
@@ -211,7 +211,7 @@ def get_hidden_prompt_for_group(message, chat_id_full, bot_name, lang, formatted
                     'you can generate images with the /img command, type /img "prompt" to generate an image for user, '
                     'you can google search with the /google command, type /google "question" to get the answer for user, '
                     'you can summarize text with the /sum command, type /sum URL to get the summary for user, '
-                    'you can say your answer with voice message with the /tts command, type /tts "text" to answer to user in voice, '
+                    'you can say your answer with voice message with the /tts command, type /tts <lang> TEXT to answer to user in voice, '
                     f'{"your special role here is " + my_db.get_user_property(chat_id_full, "role") + ", " if my_db.get_user_property(chat_id_full, "role") else ""}'
                 )
 

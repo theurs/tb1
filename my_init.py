7c4a0da2fa67083ba7b22e13f047389eea610f51
theurs <theurs@gmail.com>
2025-01-18 20:01:00 +1000

    fix system prompt

--- my_init.py
+++ my_init.py
@@ -199,6 +199,7 @@ def get_hidden_prompt_for_user(message, chat_id_full, bot_name, lang_of_user, fo
                     'you can google search with the /google command, type /google "question" to get the answer for user, '
                     'you can summarize text with the /sum command, type /sum URL to get the summary for user, '
                     'you can say your answer with voice message with the /tts command, type /tts <2 letter language code> TEXT to answer to user with voice, '
+                    "provide the best possible answer to the user's request, improvising if needed, "
                     f'{"your special role here is " + my_db.get_user_property(chat_id_full, "role") + ", " if my_db.get_user_property(chat_id_full, "role") else ""}'
                 )
 
@@ -221,6 +222,7 @@ def get_hidden_prompt_for_group(message, chat_id_full, bot_name, lang, formatted
                     'you can google search with the /google command, type /google "question" to get the answer for user, '
                     'you can summarize text with the /sum command, type /sum URL to get the summary for user, '
                     'you can say your answer with voice message with the /tts command, type /tts <2 letter language code> TEXT to answer to user with voice, '
+                    "provide the best possible answer to the user's request, improvising if needed, "
                     f'{"your special role here is " + my_db.get_user_property(chat_id_full, "role") + ", " if my_db.get_user_property(chat_id_full, "role") else ""}'
                 )
 

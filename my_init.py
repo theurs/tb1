21dab8ff63a072e5f4f9830907c61719eb09a91b
theurs <theurs@gmail.com>
2025-02-10 08:31:41 +1000

    fix

--- my_init.py
+++ my_init.py
@@ -242,7 +242,8 @@ def get_hidden_prompt_for_group(message, chat_id_full, bot_name, lang_of_user, f
                     'you can request assistance from a mathematician with /calc command, answer "/calc expression" and user will receive answer for mathematician, '
                     'you can say your answer with voice message with the /tts command, answer "/tts <2 letter language code ru|pl|en|etc> TEXT" and user will receive TEXT with voice message, '
                     'you have buttons below your messages (don`t mention it in your answer): ➡️: Continue the conversation. ♻️: Clear the chat and start over. 🙈: Hide or delete the message. 📢: TTS the text of message. ru/etc. (Language Code): Translate the message to your language, '
-                    "provide the best possible answer to the user's request, improvising if needed, "
+                    # "provide the best possible answer to the user's request, improvising if needed, "
+                    "it is important to be helpful and provide some information, even if you don't have all the details, use your best judgment to give a useful answer, "
                     f'{"your special role here is " + my_db.get_user_property(chat_id_full, "role") + ", " if my_db.get_user_property(chat_id_full, "role") else ""}'
                 )
 
@@ -267,7 +268,8 @@ def get_hidden_prompt_for_user(message, chat_id_full, bot_name, lang, formatted_
                     'you can request assistance from a mathematician with /calc command, answer "/calc expression" and user will receive answer for mathematician, '
                     'you can say your answer with voice message with the /tts command, answer "/tts <2 letter language code ru|pl|en|etc> TEXT" and user will receive TEXT with voice message, '
                     'you have buttons below your messages (don`t mention it in your answer): ➡️: Continue the conversation. ♻️: Clear the chat and start over. 🙈: Hide or delete the message. 📢: TTS the text of message. ru/etc. (Language Code): Translate the message to your language, '
-                    "provide the best possible answer to the user's request, improvising if needed, "
+                    # "provide the best possible answer to the user's request, improvising if needed, "
+                    "it is important to be helpful and provide some information, even if you don't have all the details, use your best judgment to give a useful answer, "
                     f'{"your special role here is " + my_db.get_user_property(chat_id_full, "role") + ", " if my_db.get_user_property(chat_id_full, "role") else ""}'
                 )
 

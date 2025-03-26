09dbc2521ef9a88e3a71755130f95d07a6b1c837
theurs <theurs@gmail.com>
2025-03-26 21:37:41 +1000

    fix

--- my_sum.py
+++ my_sum.py
@@ -473,7 +473,7 @@ def summ_url(url:str,
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
         try:
-            if url.lower().strip().startswith(('localhost', '127.0.0.1', 'http://localhost', 'https://localhost', 'http://127.0.0.1', 'https://127.0.0.1')):
+            if url.lower().strip().startswith(('file://', 'localhost', '127.0.0.1', 'http://localhost', 'https://localhost', 'http://127.0.0.1', 'https://127.0.0.1')):
                 if download_only:
                     return ''
                 else:

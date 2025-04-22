e875a5652d23cf3e6142a491c83b8ee676bdcecc
theurs <theurs@gmail.com>
2025-04-23 08:01:22 +1000

    fix

--- my_gemini.py
+++ my_gemini.py
@@ -316,13 +316,15 @@ def chat(query: str,
                     pass
                 elif '400 Please ensure that function response turn comes immediately after a function call turn.' in str(error):
                     my_log.log_gemini(f'my_gemini:chat2:2:4: {error}\n{model}\n\n{str(mem)}')
+                elif '429 You exceeded your current quota, please check your plan and billing details.' in str(error):
+                    my_log.log_gemini(f'my_gemini:chat2:2:5: {error}\n{model}\n\n{key}')
                 else:
                     if 'Deadline Exceeded' not in str(error) and 'stop after timeout' not in str(error) \
                     and '503 failed to connect to all addresses; last error: UNAVAILABLE' not in str(error):
                         traceback_error = traceback.format_exc()
-                        my_log.log_gemini(f'my_gemini:chat2:2:5: {error}\n{model}\n{key}\n\n{str(query)}\n{str(mem)}\n\n{traceback_error}')
+                        my_log.log_gemini(f'my_gemini:chat2:2:6: {error}\n{model}\n{key}\n\n{str(query)}\n{str(mem)}\n\n{traceback_error}')
                     else:
-                        my_log.log_gemini(f'my_gemini:chat2:2:6: {error}\n{model}\n{key}')
+                        my_log.log_gemini(f'my_gemini:chat2:2:7: {error}\n{model}\n{key}')
                 if 'reason: "CONSUMER_SUSPENDED"' in str(error) or \
                    'reason: "API_KEY_INVALID"' in str(error):
                     remove_key(key)

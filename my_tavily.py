e6746e3f847f94b89106f4fec411731cec5b9473
theurs <theurs@gmail.com>
2025-04-22 17:53:38 +1000

    fix

--- my_tavily.py
+++ my_tavily.py
@@ -34,7 +34,7 @@ def get_next_key() -> str:
 
 
 @cachetools.func.ttl_cache(maxsize=20, ttl=15*60)
-def search(query: str, max_results: int = 5, search_depth: str = 'basic') -> dict:
+def search(query: str, max_results: int = 10, search_depth: str = 'basic') -> dict:
     '''
     Делает максимальный запрос в Tavily
     

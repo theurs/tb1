68d6d649f10ad5e3d1e5e4aefb71a3b0b89f2946
theurs <theurs@gmail.com>
2025-04-29 15:43:24 +1000

    fix

--- my_tavily.py
+++ my_tavily.py
@@ -54,7 +54,7 @@ def search(
     try:
         key = get_next_key()
         query = query.strip()
-        if not key or not query or len(query) < 2:
+        if not key or not query or len(query) < 2 or len(query) > 400:
             return ''
         client = TavilyClient(key)
         if fast:

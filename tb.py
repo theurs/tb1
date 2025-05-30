c6ecef1780b068c7cd2d4ffda29448b545e47dc9
theurs <theurs@gmail.com>
2025-05-30 16:35:13 +1000

    fix

--- tb.py
+++ tb.py
@@ -2025,8 +2025,8 @@ def get_keyboard(kbd: str, message: telebot.types.Message, flag: str = '') -> te
             markup.row(button_mistral, button_llama4_maverick)
             markup.row(button_gpt_4o, button_gpt_41, button_openrouter)
             markup.row(button_cohere, button_deepseek_v3)
-            markup.row(button_filler1)
-
+            if voice_title in ('OpenAI', 'Gemini'):
+                markup.row(button_filler1)
 
             button1 = telebot.types.InlineKeyboardButton(f"{tr('📢Голос:', lang)} {voice_title}", callback_data=voice)
             if my_db.get_user_property(chat_id_full, 'voice_only_mode'):
@@ -2111,7 +2111,8 @@ def get_keyboard(kbd: str, message: telebot.types.Message, flag: str = '') -> te
                 if current_row_buttons:
                     markup.row(*current_row_buttons)
 
-            markup.row(button_filler1)
+            if voice_title in ('OpenAI', 'Gemini'):
+                markup.row(button_filler1)
 
             if my_db.get_user_property(chat_id_full, 'disabled_kbd'):
                 button1 = telebot.types.InlineKeyboardButton(tr('☑️Чат-кнопки', lang), callback_data='disable_chat_kbd')
@@ -2136,7 +2137,8 @@ def get_keyboard(kbd: str, message: telebot.types.Message, flag: str = '') -> te
                 button2 = telebot.types.InlineKeyboardButton(tr('☑️ Do not reply', lang), callback_data='send_message_chat_switch')
             markup.row(button1, button2)
 
-            markup.row(button_filler1)
+            if voice_title in ('OpenAI', 'Gemini'):
+                markup.row(button_filler1)
 
             speech_to_text_engine = my_db.get_user_property(chat_id_full, 'speech_to_text_engine') or my_stt.DEFAULT_STT_ENGINE
             if speech_to_text_engine == 'whisper':

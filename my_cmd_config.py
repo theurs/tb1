bf0f3bb35ef2bfdf45a2fea37f515903b559f767
theurs <theurs@gmail.com>
2025-09-26 14:27:06 +1000

    fix

--- my_cmd_config.py
+++ my_cmd_config.py
@@ -1,11 +1,11 @@
 # my_cmd_config.py
 
 models = [
-    ('Gemini 2.5 Flash', 'gemini25_flash'), ('Grok 4 Fast', 'cloacked'),
+    ('Gemini 2.5 Flash 🎖️', 'gemini25_flash'), ('Grok 4 Fast 🤠', 'cloacked'),
     ('GPT OSS 120b', 'gpt_oss'), ('Qwen 3', 'qwen3'),
-    ('Gemini 2.5 Pro', 'gemini15'), ('DeepSeek V3', 'deepseek_v3'),
+    ('Gemini 2.5 Flash Lite ⚡️', 'gemini-lite'), ('Gemini 2.5 Pro 👑', 'gemini15'),
     ('Qwen 3 Coder 480b', 'qwen3coder'), ('Gemini 2.0 flash', 'gemini'),
     ('GPT-4o', 'gpt-4o'), ('GPT 4.1', 'gpt_41'),
     ('Mistral', 'mistral'), ('Command A', 'cohere'),
-    ('Gemini 2.5 Flash Lite', 'gemini-lite'), ('OpenRouter', 'openrouter'),
+    ('DeepSeek V3', 'deepseek_v3'), ('OpenRouter ⚙️', 'openrouter'),
 ]
